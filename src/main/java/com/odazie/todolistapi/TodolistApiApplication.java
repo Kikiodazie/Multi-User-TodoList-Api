cecd5550ffb03d83b454ec38675d7c1535471c96
Kikiodazie <kiki.odazie@yahoo.com>
2020-09-05 02:04:39 +0100

    Initial commit: Set up project.

---
+++ src/main/java/com/odazie/todolistapi/TodolistApiApplication.java
@@ -0,0 +1,13 @@
+package com.odazie.todolistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodolistApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/odazie/todolistapi/TodolistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odazie.todolistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
