8bbb3f126b254a2d9d82e2c0990468e6464db6de
Kikiodazie <kiki.odazie@yahoo.com>
2020-10-22 03:00:06 +0100

    heroku

---
+++ src/main/java/com/odazie/todolistapi/data/DatabaseConfig.java
@@ -0,0 +1,22 @@
+package com.odazie.todolistapi.data;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public HikariDataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
+
